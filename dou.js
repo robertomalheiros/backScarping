a5767982d573664f3dbf46e3652ca2a69207dae6
robertomalheiros <robertomalheirosb@gmail.com>
2022-12-10 13:56:57 -0300

    Scraping DOU - FALTA REGRA PARA NÃO GERAR DIÁRIO REPETIDO

--- dou.js
+++ dou.js
@@ -3,16 +3,20 @@ import { v1 } from "uuid";
 import { join } from "path";
 import download from "download";
 import fs from "fs";
+import axios from "axios";
+
+//SE TYPE NÃO FOR MODULE
 //const { Cluster } = require("puppeteer-cluster");
 //const puppeteer = require("puppeteer");
 //const { join } = require("path");
 //const download = require("download");
 //const fs = require("fs");
 
+//URL DO DIÁRIO
 const url =
   "https://in.gov.br/servicos/diario-oficial-da-uniao/destaques-do-diario-oficial-da-uniao?p_p_id=com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_mhF1RLPnJWPh&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_mhF1RLPnJWPh_delta=20&p_r_p_resetCur=false&_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_mhF1RLPnJWPh_cur=2";
 
-let dados = [];
+//Função de Scraping
 async function render({ page }) {
   console.log("2-Executando Render");
   // const browser = await puppeteer.launch();
@@ -35,21 +39,34 @@ async function render({ page }) {
       let imagem = diarios.querySelector(".col-2 > a > img").src;
       let pdf = diarios.querySelector(".col-2 > a").href;
       let dataPublicacao = diarios.querySelector(".date").textContent;
+      //dados.push({ titulo, orgao, imagem, pdf, dataPublicacao });
+
       return { titulo, orgao, imagem, pdf, dataPublicacao };
     });
     return diario_info;
   });
 
-  //Criando um arquivo JSON com os dados buscados
-  fs.writeFile("dados", JSON.stringify(dou_detalhes, null, 2), (err) => {
-    if (err) throw new Error("Erro ao criar arquivo.");
-  });
+  // //Criando um arquivo JSON com os dados buscados
+  // fs.writeFile("dados", JSON.stringify(dou_detalhes, null, 2), (err) => {
+  //   if (err) throw new Error("Erro ao criar arquivo.");
+  // });
 
-  // [...dados] = dou_detalhes;
+  //FAZENDO POST NA COLLECTION DOCUMENTOS
+  async function conect() {
+    try {
+      for (const item of dou_detalhes) {
+        await axios.post("https://reader-gov-back.cyclic.app/documents", item);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  conect();
+
   console.log("4-Finalidando o Render");
   await browser.close();
 }
-
+//FUNÇÃO PARA GERAR PDFs
 async function gerarPDF({ page, data: { urlPDF } }) {
   console.log("Entrando na função gerarPDFs");
   await page.goto(urlPDF, { waitUntil: "networkidle2" });
@@ -66,6 +83,7 @@ async function gerarPDF({ page, data: { urlPDF } }) {
   console.log("PDF gerado!", output);
 }
 
+//FUNÇÃO MAIN PARA GERAR CLUSTER DO PUPPETEER
 async function main() {
   const pid = process.pid;
   try {
--- index.js
+++ index.js
@@ -3,44 +3,47 @@ import * as dotenv from "dotenv";
 const app = express();
 import dou from "./dou.js";
 import path from "path";
-//import cp from "child-process";
-//import dados from "./dados.json";
+
 import fs from "fs";
 import bodyParser from "body-parser";
 import schedule from "node-schedule";
 
-const caminho = "./dados.json";
+//CAMINHO PARA LEITURA DE DADOS NO DISCO
+//const caminho = "./dados.json";
 
-let diarios = "";
+//let diarios = "";
 
+//CRIANDO REGRA PARA AGENDAR SCRAPING
 const regra = new schedule.RecurrenceRule();
 regra.dayOfWeek = [new schedule.Range(1, 5)];
 regra.hour = 2;
 regra.second = 30;
-
+//AGENDANDO SCRAPING PARA 2:00
 const scraping_dou = schedule.scheduleJob(regra, async function () {
   console.log("Realizando Scrapimg DOU diário.");
   await dou();
   console.log("Scraping DOU realizado..");
 });
 
+//CHAMANDO CLUSTER PARA REALIZAR O PRIMEIRO SCRAPING
 (async function () {
   await dou();
-  console.log("Continua executando Server na porta 8080.");
+  console.log("Continua executando Server na porta 8081.");
 })();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-fs.readFile(caminho, "utf-8", (err, conteudo) => {
-  diarios = JSON.parse(conteudo);
+//LENDO JSON DO DISCO
+//fs.readFile(caminho, "utf-8", (err, conteudo) => {
+//  diarios = JSON.parse(conteudo);
+//});
+
+app.get("/", (req, res, next) => {
+  res.send("Scraping Funcionando!");
 });
 
-app.get("/dou", (req, res, next) => {
-  res.send(diarios);
-});
-
-app.listen(8080, () => {
-  console.log("Server na porta 8080.");
+app.listen(8081, () => {
+  console.log("Server na porta 8081.");
 });
